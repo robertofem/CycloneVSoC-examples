b3437e6119ff622a9262aeaddad3c7097467dbce
robertofem <robertofem@gmail.com>
2017-06-01 20:08:14 +0200

    Modified the Secon Counter example

---
+++ DE1-SoC/Baremetal/Second_Counter_PMU/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "pmu.h"
+int __auto_semihosting;
+int main(void)
+{
+    printf("Hello World!!\r\n");
+	
+	pmu_init_ns(800, 1); //Initialize PMU cycle counter, 800MHz source, frequency divider 1
+	pmu_counter_enable();//Enable cycle counter inside PMU (it starts counting)
+	float pmu_res = pmu_getres_ns();
+	printf("PMU is used like timer with the following characteristics\n\r");
+	printf("PMU cycle counter resolution is %f ns\n\r", pmu_res );	
+	
+	//Timing 60 seconds with PMU
+	int seconds=0;
+	unsigned long long pmu_counter_ns;
+	
+	pmu_counter_reset();
+	printf("%d\n\r",seconds);
+	
+	while (seconds<61)
+	{
+		pmu_counter_read_ns(&pmu_counter_ns);
+		if(pmu_counter_ns>1000000000)
+		{
+			pmu_counter_reset();
+			seconds ++;
+			printf("%d\n\r",seconds);
+		}
+	}
+
+	printf("The end...\n\r");
+    return 0;
+}
